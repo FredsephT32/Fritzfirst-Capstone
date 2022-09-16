fa96c2329bc02ac378f0de1d9043f4dee76600e9
FredsephT32 <106403769+FredsephT32@users.noreply.github.com>
2022-09-15 20:53:43 -0400

    ESLint Line 57 Report solution Attempt 1

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const speakerCards = [
 
 function setSpeakerContainerChildren() {
   const SpeakerContainer = document.querySelector('section.features-container');
-  for(let i = 0; i < speakerCards.length; i += 1) {
+  for (let i=0; i<speakerCards.length; i+=1) {
 
     // Create features parent (div)
     const feature = document.createElement('div');
